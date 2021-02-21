11001cd5b1a2629aa71febf11e6952025e721703
Arjun Jaishi <a.jaishi7777777@gmail.com>
2021-02-22 02:08:34 +0545

    User Route | Find Me

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const {register, findMe, findUs, removeMe, updateMe} = require('../controller/auth');
 router.post('/register', register);
+router.get('/findMe/:id', findMe);
 
 
 module.exports = router;
