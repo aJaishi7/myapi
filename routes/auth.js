186bacae4daf7835416d96c2f4ae5c7c160b7105
Arjun Jaishi <a.jaishi7777777@gmail.com>
2021-02-22 02:05:49 +0545

    User Route | Register User

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const {register, findMe, findUs, removeMe, updateMe} = require('../controller/auth');
+router.post('/register', register);
 
 
 module.exports = router;
