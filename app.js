85c741981a3f4dac7d8eeade970802541b1dd1be
Arjun <a.jaishi7777777@gmail.com>
2021-02-21 14:12:11 +0545

    Entry Point of App

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const colors = require('colors');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: "./config/app.env",
+
+});
+
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST;
+app.listen(PORT,
+    () =>
+        console
+            .log(`\n*****Server Started*****\n   Host : ${HOST}\n   Mode:${process.env.NODE_ENV}\n   PORT: ${PORT}`.yellow.bold)
+);
